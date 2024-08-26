bd285e168d2fe3bce9487107d5e369c385e9211b
varelam-bzz <varelam@bzz.ch>
2024-08-26 11:48:02 +0200

    Aufgabe erledigt

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-# Dein Code kommt hier hin
+""" LU2.A08 """
+import json
+
+
+def adjust_recipe(original_recipe, num_people):
+    adjusted_ingredients = {}
+    original_servings = original_recipe['servings']
+    factor = num_people / original_servings
+
+    for ingredient, amount in original_recipe['ingredients'].items():
+        adjusted_ingredients[ingredient] = amount * factor
+
+    adjusted_recipe = {
+        'title': original_recipe['title'],
+        'ingredients': adjusted_ingredients,
+        'servings': num_people,
+    }
+    return adjusted_recipe
+
+
+def load_recipe(original_recipe):
+    return json.loads(original_recipe)
+
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    orig_recipe = json.loads(recipe_json)
+    adj_recipe = adjust_recipe(orig_recipe, 2)
